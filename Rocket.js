cefafbed7636fe4fa8b7ed6a961f0914ed0fdf1e
doev03 <doev03@github.com>
2020-12-27 22:44:48 +0300

    da

--- Rocket.js
+++ Rocket.js
@@ -129,8 +129,8 @@ $(document).ready(function () {
         dot.style.left = - dot.width / 2 + rocket.offsetLeft + rocket.width / 2 + (rocket.height / 2 + 5) * Math.sin(rocketAngle * (Math.PI / 180)) + "px";
         let dotAngle = rocketAngle;
         dot.style.position = "absolute";
-        dot.style.zIndex = "1000";
         document.body.append(dot);
+        dot.style.zIndex = 1000;
 
         let currDotTicks = new Date().getTime();
         let prevDotTicks;
@@ -165,7 +165,7 @@ $(document).ready(function () {
         elem.style.top = elem.offsetTop + "px";
         elem.style.left = elem.offsetLeft + "px";
         elem.style.position = "absolute";
-        elem.style.zIndex = "1000";
+        elem.style.zIndex = 1000;
         let currElementTicks = new Date().getTime();
         let prevElementTicks;
 
@@ -235,8 +235,8 @@ $(document).ready(function () {
         rocket.style.left = 30 + "px";
         rocket.style.top = 30 + "px";
         rocket.style.position = "absolute";
-        rocket.style.zIndex = "1000";
         document.body.append(rocket);
+        rocket.style.zIndex = 1000;
 
         createAsteroids();
 
